3e5d458818a2be2b16c71376964df80ec1e3a7a5
Max <max@aiwon.ru>
2021-11-03 19:44:34 +0300

    fix middleware paths #2

--- pages/add-tailwind-to-next.ts/_middleware.ts
+++ pages/add-tailwind-to-next.ts/_middleware.ts
@@ -2,10 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { SITE_NAME } from "../../lib/constants";
 
 export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
+  // const { pathname } = req.nextUrl;
   if ((req.ua! as any).browser.name) {
     return NextResponse.next();
   }
   // return NextResponse.redirect("http://localhost:3000/files" + pathname);
-  return NextResponse.rewrite(String(SITE_NAME) + "/files" + pathname);
+  // return NextResponse.rewrite(String(SITE_NAME) + "/files" + pathname);
+  return NextResponse.rewrite(
+    String(SITE_NAME) + "/files" + "/add-tailwind-to-next.ts"
+  );
 }
