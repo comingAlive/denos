fd15982b0ff638e0f8f5c03154afdf3499f44adc
Max <max@aiwon.org>
2023-01-27 16:12:33 +0300

    update to 13 and denos.cc;

--- next.config.js
+++ next.config.js
@@ -8,17 +8,17 @@ module.exports = {
   //   serverComponents: true,
   // },
   i18n: { locales: ["en"], defaultLocale: "en" },
-  webpack(config, { dev, isServer }) {
-    // Replace React with Preact in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-    return config;
-  },
+  // webpack(config, { dev, isServer }) {
+  //   // Replace React with Preact in client production build
+  //   if (!dev && !isServer) {
+  //     Object.assign(config.resolve.alias, {
+  //       react: "preact/compat",
+  //       "react-dom/test-utils": "preact/test-utils",
+  //       "react-dom": "preact/compat",
+  //     });
+  //   }
+  //   return config;
+  // },
   async rewrites() {
     return [
       {
