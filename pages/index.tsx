a7a28141a19de3bb888b73d0c71bd845c9a7798a
Max <max@aiwon.org>
2023-02-02 14:49:45 +0300

    switch to denos cc and pnpm;

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { CommandBlockWithTitle, Container } from "../components";
-import { supabase } from "../lib/supabaseClient";
-import { useEffect } from "react";
 import useSubmitVisitor from "../hooks/useSubmitVisitor";
+import Head from "next/head";
 
 export default function IndexPage() {
   useSubmitVisitor();
@@ -10,6 +9,29 @@ export default function IndexPage() {
       title="Collection of Deno Scripts"
       description="Collection of Deno Scripts"
     >
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <article className="mt-10">
         <h2 id="1">Add Tailwind CSS to Next.js project</h2>
         <CommandBlockWithTitle
